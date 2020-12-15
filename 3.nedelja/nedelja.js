5b9532a3903753635aa7a65bf0106dc19d08f651
Ivann-2612 <ivan.nikolicamp@gmail.com>
2020-12-15 21:09:21 +0100

    uradjen domaci

--- 3.nedelja/nedelja.js
+++ 3.nedelja/nedelja.js
@@ -1,1 +1,27 @@
-//domaci
+//1. Написати функцију која исписује све елементе низа који су дељиви са 5 
+function divisibleByFive(arr) {
+    for (i = 0; i < arr.length; i++) {
+        //console.log(array[i]);
+      if (arr[i] % 5 == 0) {
+        console.log(arr[i]);
+      }
+    }
+  }
+  console.log(divisibleByFive([1,2,3,4,5,10,15,23,1,5]));  // Исписује -> 5,10,15,5
+
+  console.log('-----------------------------------------');
+
+  //Drugi nacin pomocu filter metode
+  let arr = [1,2,3,4,5,10,15,23,1,5]
+  const isDivisibleByFive = arr => arr.filter(i => i % 5 == 0);
+  console.log(isDivisibleByFive(arr));
+  
+//2. Написати функцију која враћа колико пице вам је довољно за доживотно снабдевање
+// Уноси се колико пице поједете на месечном нивоу, и колико година имате (Рачуна се да је довољно достављати до стоте године)
+
+function lifeSupply(numPerMonth,age){
+    
+}
+
+// lifeSupply(10,26) // Враћа 8880
+// console.log(lifeSupply(10,26)) 
--- 3.nedelja/sreda.js
+++ 3.nedelja/sreda.js
@@ -1,1 +1,20 @@
-//domsaci
+//1. Написати функцију која за параметар прима број сати (0-24) и у зависности од доба дана исписује поруке: Добро јутро , Добар дан или Добро вече.
+function brojSati(a) {
+    if(a >=0 && a<= 12){
+        console.log('Dobro jutro');
+    }else if(a >=12 && a < 18){
+        console.log('Dobar dan');
+    }else{
+        console.log('Dobro vece');
+    }
+}
+console.log(brojSati(19));
+//2. Написати функцију која израчунава цену пице по квадратном центиметру (параметри функције су полупречник у цм, и цена пице)
+function cenaPicePoCm2(cenaPice,poluprecnikUCm) {
+    let R = 32
+    poluprecnikUCm = R/2
+    let P = (poluprecnikUCm**2) * Math.PI
+    return P/cenaPice
+}
+
+console.log(cenaPicePoCm2(1000));
